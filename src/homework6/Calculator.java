b92211340b1485d947bcd9b1bbcb23ca47aa4d3c
valeriiashyshko <lera.sh95@gmail.com>
2023-10-09 16:00:45 +0200

    Homework 6

---
+++ src/homework6/Calculator.java
@@ -0,0 +1,24 @@
+package homework6;
+
+public class Calculator {
+
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public static int division(int a, int b) {
+        if (b != 0)
+            return a / b;
+        else
+            System.out.println("Error! Division by 0 is impossible");
+        throw new ArithmeticException("Division by zero");
+    }
+}
