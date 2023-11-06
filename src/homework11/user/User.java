efada653117a0c57be8e7e7fae464b5c7b69a9b4
valeriiashyshko <lera.sh95@gmail.com>
2023-11-06 22:22:02 +0100

    1-2. Suffered while creating a lambda expression; 3. Created 10 user objects, used streams to check each operation.

---
+++ src/homework11/user/User.java
@@ -0,0 +1,45 @@
+package homework11.user;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private String firstName;
+    private String secondName;
+    private int age;
+
+    public User(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "User: " + firstName + " " + secondName + " " + age + " y.o.";
+    }
+}
